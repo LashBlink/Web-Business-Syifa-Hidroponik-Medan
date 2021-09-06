62c8437dc7951bf7a9400b3fbffedb3c55054ab2
Awaludin-Pamungkas <awalks141@gmail.com>
2021-09-07 06:42:15 +0700

    tambah landing-page

---
+++ app/Views/home/landing_page.php
@@ -0,0 +1,264 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="/css/style.css">
+    <title>Syifa Hidroponik</title>
+</head>
+
+<body>
+    <!--navbar-->
+    <section id="nav">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container">
+                <a class="navbar-brand" href="#">Navbar</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Produk
+                            </a>
+                            <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                <li><a class="dropdown-item" href="#">Produk Olahan</a></li>
+                                <li><a class="dropdown-item" href="#">Peralatan hidroponik</a></li>
+                            </ul>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Tentang kami
+                            </a>
+                            <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                <li><a class="dropdown-item" href="#">Tentang Syifa Hidroponik</a></li>
+                                <li><a class="dropdown-item" href="#">Visi & Misi</a></li>
+                                <li><a class="dropdown-item" href="#">Sejarah</a></li>
+                            </ul>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Sosial Media</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </section>
+
+    <!-- Banner -->
+    <section id="banner">
+        <div class="banner">
+            <div class="container">
+                <div class="row">
+                    <div class="col">
+                        <h1>Syifa Hidroponik Medan</h1>
+                        <hr>
+                        <p>Sebuah tempat yang menyediakan perlengkapan dan
+                            segala macam kebutuhan untuk berkebun dengan sistem hidroponik. </p>
+                        <div class="btn-group">
+                            <a herf="#">
+                                <button type="button" class="btn banner">Lihat</button>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col">
+                        2 of 2
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>
+
+    <!-- produk& layanan -->
+    <section class="features-icons bg-light text-center">
+        <div class="container">
+            <div>
+                <h1>Produk dan Layanan</h1>
+                <br>
+                <p>Produk & Layanan Kami meliputi beberapa kegiatan usaha diantaranya</p>
+                <br>
+            </div>
+            <div class="row">
+                <div class="col-lg-4">
+                    <div class="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
+                        <div class="features-icons-icon d-flex"><img src="/assets/img/jpg1.jpg" class="card-img-top" alt="..."></i></div>
+                        <br>
+                        <h2>Peralatan Hidroponik</h2>
+                        <p class="lead mb-0">This theme will look great on any device, no matter the size!</p>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
+                        <div class="features-icons-icon d-flex"><img src="/assets/img/jpg1.jpg" class="card-img-top" alt="..."></i></div>
+                        <br>
+                        <h2>Produk Olahan</h2>
+                        <p class="lead mb-0">Featuring the latest build of the new Bootstrap 5 framework!</p>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="features-icons-item mx-auto mb-0 mb-lg-3">
+                        <div class="features-icons-icon d-flex"><img src="/assets/img/jpg1.jpg" class="card-img-top" alt="..."></i></div>
+                        <br>
+                        <h2>Pemasaran</h2>
+                        <p class="lead mb-0">Ready to use with your own content, or customize the source files!</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- hidroponik? -->
+    <section>
+        <div class="card mb-3">
+            <div class="row g-0">
+                <div class="col-md-4">
+                    <img src="/assets/img/jpg1.jpg" class="img-fluid rounded-start" alt="...">
+                </div>
+                <div class="col-md-8 bg-dark ">
+                    <div class="card-body" style="color: azure;">
+                        <span style=" vertical-align: middle;">
+                            <h1>Apa itu Hidroponik?</h1>
+                            <hr class=" bg-light">
+                            <p class="card-text" style="font-size: 32px;">Hidroponik adalah metode menanam tanaman
+                                tanpa menggunakan tanah. Akar tanaman akan menyerap
+                                unsur hara yang diperlukan untuk pertumbuhan melalui
+                                larutan nutrisi yang disebut dengan AB mix.</p>
+                            <br>
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- youtube-chanel -->
+    <section>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg">
+
+                    <div class="embed-responsive embed-responsive-16by9">
+                        <span class="align-middle"> <iframe width="560" height="315" src="https://www.youtube.com/embed/pMk24G6b540" allowfullscreen></iframe>
+                    </div>
+
+                </div>
+                <div class="col-lg">
+                    <span style=" vertical-align: middle;">
+                        <h1>Free video Tutorial</h1>
+                        <hr class=" bg-light">
+                        <p class="card-text" style="font-size: 32px;">Ikuti kami di Youtube untuk melihat perkembangan kami
+                            dan dapatkan video tutorial gratis cara memanan hidroponik.</p>
+                        <a href="">
+                            <img class="subscribe" src="https://freesvg.org/img/Subscribe.png">
+                        </a>
+
+                    </span>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="features-icons bg-light">
+        <div class="container">
+            <div>
+                <h1>Lastest Blog Post</h1>
+            </div>
+            <br>
+
+            <div class="card mb-3">
+                <div class="row g-0">
+                    <div class="col-md-4">
+                        <img src="/assets/img/jpg1.jpg" class="img-fluid rounded-start" alt="...">
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <h5 class="card-title">Post title</h5>
+                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </section>
+
+    <!-- Footer -->
+    <footer class="footer bg-green">
+        <div class="container">
+            <div class="row mt-5 mb-5">
+                <div class="col-lg-6 h-100 text-center text-lg-start my-auto">
+                    <!-- <ul class="list-inline mb-2">
+                        <li class="list-inline-item"><a href="#!">About</a></li>
+                        <li class="list-inline-item">⋅</li>
+                        <li class="list-inline-item"><a href="#!">Contact</a></li>
+                        <li class="list-inline-item">⋅</li>
+                        <li class="list-inline-item"><a href="#!">Terms of Use</a></li>
+                        <li class="list-inline-item">⋅</li>
+                        <li class="list-inline-item"><a href="#!">Privacy Policy</a></li>
+                    </ul> -->
+                    <p class="text-muted small mb-4 mb-lg-0">JL. Bromo Lr. Amal No. 11
+                        Medan 20216
+                    </p>
+                    <hr>
+                    <table>
+                        <tr>
+                            <td>Telp</td>
+                            <td> : </td>
+                            <td>0812 6011 332
+                                / 0813 7084 8955</td>
+                        </tr>
+                        <tr>
+                            <td>E-mail</td>
+                            <td> : </td>
+                            <td>r4d3n92@gmail.com</td>
+                        </tr>
+                    </table>
+
+
+                </div>
+                <div class="col-lg-6 h-100 text-center text-lg-end my-auto">
+                    <ul class="list-inline mb-0">
+                        <li class="list-inline-item me-4">
+                            <a href="#!"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/09/YouTube_full-color_icon_%282017%29.svg/800px-YouTube_full-color_icon_%282017%29.svg.png" class="icon-footer"></i></a>
+                        </li>
+                        <li class="list-inline-item me-4">
+                            <a href="#!"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Instagram_icon.png/1024px-Instagram_icon.png" class="icon-footer-squer"></i></a>
+                        </li>
+                        <li class="list-inline-item">
+                            <a href="#!"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/Facebook_icon.svg/1200px-Facebook_icon.svg.png" class="icon-footer-squer"></i></a>
+                        </li>
+                    </ul>
+                    <ul class="list-inline mb-0">
+                        <p class="text-muted small mb-4 mb-lg-0">&copy; Your Website 2021. All Rights Reserved.</p>
+                    </ul>
+
+                </div>
+            </div>
+        </div>
+    </footer>
+
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    -->
+</body>
+
+</html>
