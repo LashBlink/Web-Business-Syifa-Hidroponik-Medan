3d7ee088ceef282e5808a9489b057de01bba3643
LashBlink <fashaade1234@gmail.com>
2021-09-08 14:41:36 +0700

    login

--- app/Views/admin/login.php
+++ app/Views/admin/login.php
@@ -1,11 +1,60 @@
-<div class="login-page">
-    <div class="form">
-        <form class="register-form">
-            <input type="text" placeholder="name" />
-            <input type="password" placeholder="password" />
+<!doctype html>
+<html lang="en">
 
-            <button>login</button>
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+
+    <title>login</title>
+</head>
+
+<body>
+
+    <style>
+        input[type="username"] {
+            border-bottom-left-radius: 0px;
+            border-bottom-right-radius: 0px;
+        }
+
+        input[type="password"] {
+            border-top-left-radius: 0px;
+            border-top-right-radius: 0px;
+
+        }
+    </style>
+
+
+    <div class="text-center">
+        <form style="max-width: 400px; margin:auto;">
+            <img class="mt-4 mb-4" src="/assets/img/logo.png" height="72">
+            <h1 class="h3 mb-3 font-weight-normal">Please Login</h1>
+            <label for="username" class="sr-only"></label>
+            <input type="text" id="username" class="form-control" placeholder="username" required autofocus>
+            <label for="password" class="sr-only"></label>
+            <input type="password" id="password" placeholder="password" class="form-control">
+
+            <div class="mt-3">
+                <button type="button" class="btn btn-lg btn-success btn-block">Login</button>
+            </div>
         </form>
     </div>
-</div>
+
+
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js" integrity="sha384-W8fXfP3gkOKtndU4JGtKDvXbO53Wy8SZCQHczT5FMiiqmQfUpWbYdTil/SxwZgAN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.min.js" integrity="sha384-skAcpIdS7UcVUC05LJ9Dxay8AXcDYfBJqt1CJ85S/CFujBsIzCIv+l9liuYLaMQ/" crossorigin="anonymous"></script>
+    -->
+</body>
+
+</html>
