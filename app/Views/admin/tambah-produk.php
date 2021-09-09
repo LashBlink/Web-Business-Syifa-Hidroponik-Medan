25efa608ff5e5a3e86e50a0de47cd48bb36e917e
Awaludin-Pamungkas <awalks141@gmail.com>
2021-09-09 11:27:51 +0700

    halaman tambah produk

---
+++ app/Views/admin/tambah-produk.php
@@ -0,0 +1,55 @@
+<?= $this->extend('layout/admintemplate'); ?>
+
+<?= $this->section('content'); ?>
+<div class="container-fluid">
+    <div class="row flex-nowrap">
+        <?= $this->include('layout/sidebar'); ?>
+        <!-- content -->
+        <div class="col py-3">
+            <div>
+                <p class="fs-3">Tambah Produk
+                    <hr>
+                <div class="row my-1">
+                    <div class="col-2">
+                        <p>Nama Produk</p>
+                    </div>
+                    <div class="col-3">
+                        <input class="form-control" type="type">
+                    </div>
+                </div>
+                <div class="row my-1">
+                    <div class="col-2">
+                        <p>Gambar Produk</p>
+                    </div>
+                    <div class="col-3">
+                        <input class="form-control" type="file">
+                    </div>
+                </div>
+                <div class="row my-1">
+                    <div class="col-2">
+                        <p>Katagori Produk</p>
+                    </div>
+                    <div class="col-3">
+                        <input class="form-control" type="text">
+                    </div>
+                </div>
+                <div class="row my-1">
+                    <div class="col-2">
+                        <p>Deskripsi Produk</p>
+                    </div>
+                    <div class="col-3">
+                        <input class="form-control" type="text">
+                    </div>
+                </div>
+                <div class="row mt-5">
+                    <div class="cols">
+                        <button type="button" class="btn btn-primary">Tambah</button>
+                        <button type="button" class="btn btn-danger">hapus</button>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
+<?= $this->endsection(); ?>
